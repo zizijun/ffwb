33c29a035e890ee925b04f9760eb16b8bc297a9c
WangLei <1016412963@qq.com>
2017-07-17 09:27:50 +0800

    更新上传文件

---
+++ src/main/java/com/ffwb/dao/AnalysisDao.java
@@ -0,0 +1,11 @@
+package com.ffwb.dao;
+
+import com.ffwb.entity.Analysis;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by lenovo on 2017/7/15.
+ */
+public interface AnalysisDao extends JpaRepository<Analysis,Long> {
+
+}
